7280dde57f64c9e7e38c87d35414f0e3f82313e9
Roxana <ms.roxanarahimi@gmail.com>
2025-11-10 03:28:03 +0330

    aa

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -9,4 +9,12 @@ class Customer extends Model
     protected $connection = 'sqlsrv';
     protected $table = 'SLS3.Customer';
     protected $hidden = ['Version'];
+    public function CustomerAddress()
+    {
+        return $this->hasOne(CustomerAddress::class, 'CustomerRef','CustomerID');
+    }
+    public function Party()
+    {
+        return $this->hasOne(Party::class, 'PartyID','PartyRef');
+    }
 }
